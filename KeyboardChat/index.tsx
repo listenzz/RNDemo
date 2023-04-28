93cbd264be1e1eb9afadcc98f15803e7158dd3cd
listen <listenzz@163.com>
2023-04-28 11:44:40 +0800

    更新 demo

--- KeyboardChat/index.tsx
+++ KeyboardChat/index.tsx
@@ -14,7 +14,7 @@ import Message from './Message'
 import { history } from './Message/data'
 import styles from './styles'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
-import { KeyboardInsetsView, getEdgeInsetsForView } from 'react-native-keyboard-insets'
+import { KeyboardInsetsView, getEdgeInsetsForView } from '@sdcx/keyboard-insets'
 import { ViewDriver } from './driver/ViewDriver'
 import { Driver } from './driver/Driver'
 import { KeyboardDriver } from './driver/KeyboardDriver'
@@ -64,7 +64,7 @@ export default function KeyboardChat() {
           </View>
         </ScrollView>
         <View style={styles.sender} ref={senderRef} onLayout={onLayout}>
-          <TextInput ref={inputRef} style={styles.input} />
+          <TextInput ref={inputRef} style={styles.input} multiline textAlignVertical="center" />
           <Pressable
             style={styles.button}
             onPress={() => (emoji.shown ? keyboard.show() : emoji.show(driverState))}>
--- KeyboardInsets/index.tsx
+++ KeyboardInsets/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, TextInput, ScrollView } from 'react-native'
-import { KeyboardInsetsView } from 'react-native-keyboard-insets'
+import { KeyboardInsetsView } from '@sdcx/keyboard-insets'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 
 export default function KeyboardInsets() {
